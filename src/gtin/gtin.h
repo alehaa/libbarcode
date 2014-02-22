234b04fda6fbaf5c6cae90f521bea7f9758b3e51
Alexander Haase <alexander.haase@rwth-aachen.de>
2014-02-22 15:55:16 +0100

    renaming

--- src/gtin/gtin.h
+++ src/gtin/gtin.h
@@ -1,6 +1,6 @@
-/* This file is part of libgtin.
+/* This file is part of libbarcode.
  *
- * libgtin is free software: you can redistribute it and/or modify it under the
+ * libbarcode is free software: you can redistribute it and/or modify it under the
  * terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation, either version 3 of the License, or (at your option) any
  * later version.
